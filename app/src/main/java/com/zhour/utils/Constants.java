6a2e779bc1ea2bb7137b2912969cf3cacd555c6e
shankar <shankar.pilli@sparity.com>
2017-07-05 21:04:17 +0530

    Initial Commit

---
+++ app/src/main/java/com/zhour/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.zhour.utils;
+
+/**
+ * Created by Shankar on 6/29/2017.
+ */
+
+public class Constants {
+    // Log message On or Off
+    public static final boolean logMessageOnOrOff = true;
+
+    /* shared pref */
+    public static final String APP_PREF = "Zhour_Pref";
+}
---
+++ app/src/main/java/com/zhour/utils/Utility.java
@@ -0,0 +1,243 @@
+package com.zhour.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+/**
+ * Created by Shankar on 6/29/2017.
+ **/
+
+public class Utility {
+
+
+    /**
+     * TO CHECK IS IT BELOW MARSHMALLOW OR NOT
+     */
+    public static boolean isMarshmallowOS() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    /**
+     * ASSIGN THE COLOR
+     **/
+    public static int getColor(Context context, int id) {
+        final int version = Build.VERSION.SDK_INT;
+        if (version >= 23)
+            return ContextCompat.getColor(context, id);
+        else
+            return context.getResources().getColor(id);
+    }
+
+
+    /**
+     * FONT AWESOME WEB FONT TYPEFACE
+     * This method is used to set the icons in font awesome
+     **/
+    public static Typeface getFontAwesomeWebFont(Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "fonts/fontawesome-webfont.ttf");
+    }
+
+    /**
+     * MATERIAL ICONS REGULAR TYPEFACE
+     * This method is used to set the icons in Material Icons Regular
+     **/
+    public static Typeface getMaterialIconsRegular(Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "fonts/matireal_icons_regular.ttf");
+    }
+
+    /**
+     * FOUNDATION ICONS TYPEFACE
+     * This method is used to set the icons in Foundation Icons
+     **/
+    public static Typeface getFoundationIcons(Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "fonts/foundation-icons.ttf");
+    }
+
+    /**
+     * MOON ICONS TYPEFACE
+     * This method is used to set the icons in Foundation Icons
+     **/
+    public static Typeface getMoonIcons(Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "fonts/moon.ttf");
+    }
+
+
+    /**
+     * HIDE THE KEYBOARD FOR FRAGMENT
+     **/
+    public static void hideSoftKeyboard(Activity activity, View view) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getApplicationWindowToken(), 0);
+    }
+
+    /**
+     * HIDE THE KEYBOARD FOR ACTIVITY
+     **/
+    public static void hideSoftKeyPad(Context context) {
+        Activity activity = (Activity) context;
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * GET REAL PATH FROM URI
+     *
+     * @param contentUri Uri of the real path
+     * @return String
+     */
+    public static String _getRealPathFromURI(Context context, Uri contentUri) {
+        String[] proj = {MediaStore.Audio.Media.DATA};
+        CursorLoader loader = new CursorLoader(context, contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+    /**
+     * GET Resources String
+     *
+     * @param context Context of the class
+     * @param id      Id of the resource
+     * @return String
+     */
+    public static String getResourcesString(Context context, int id) {
+        String value = null;
+        if (context != null && id != -1) {
+            value = context.getResources().getString(id);
+        }
+        return value;
+    }
+
+    /**
+     * Check the value is null or empty
+     *
+     * @param value Value of that string
+     * @return Boolean returns the value true or false
+     */
+    public static boolean isValueNullOrEmpty(String value) {
+        boolean isValue = false;
+        if (value == null || value.equals("") || value.equals("0.0")
+                || value.equals("null") || value.trim().length() == 0) {
+            isValue = true;
+        }
+        return isValue;
+    }
+
+    /**
+     * Shows toast message
+     *
+     * @param context Context of the class
+     * @param message What message you have to show
+     */
+    public static void showToastMessage(Context context, String message) {
+        try {
+            if (!isValueNullOrEmpty(message) && context != null) {
+                final Toast toast = Toast.makeText(
+                        context.getApplicationContext(), message,
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * To Print logcat this method is used.
+     *
+     * @param logMsg Purpose of the log
+     * @param logVal What you want to print
+     */
+    public static void showLog(String logMsg, String logVal) {
+        try {
+            if (Constants.logMessageOnOrOff) {
+                if (!isValueNullOrEmpty(logMsg) && !isValueNullOrEmpty(logVal)) {
+                    Log.e(logMsg, logVal);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * ASSIGN THE DRAWBLE
+     **/
+    public static Drawable getDrawable(Context context, int id) {
+        final int version = Build.VERSION.SDK_INT;
+        if (version >= 21) {
+            return ContextCompat.getDrawable(context, id);
+        } else {
+            return context.getResources().getDrawable(id);
+        }
+    }
+
+    /**
+     * Shared preference method to set and get boolean variable
+     */
+    public static void setSharedPrefBooleanData(Context context, String key, boolean value) {
+        SharedPreferences appInstallInfoSharedPref = context.getSharedPreferences(Constants.APP_PREF,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor appInstallInfoEditor = appInstallInfoSharedPref.edit();
+        appInstallInfoEditor.putBoolean(key, value);
+        appInstallInfoEditor.apply();
+    }
+
+    public static boolean getSharedPrefBooleanData(Context context, String key) {
+        SharedPreferences userAccountPreference = context.getSharedPreferences(Constants.APP_PREF, Context.MODE_PRIVATE);
+        return userAccountPreference.getBoolean(key, false);
+    }
+
+    /**
+     * Sharedpreference method to set and get string variable
+     */
+    public static void setSharedPrefStringData(Context context, String key, String value) {
+        try {
+            if (context != null) {
+                SharedPreferences appInstallInfoSharedPref = context.getSharedPreferences(Constants.APP_PREF,
+                        Context.MODE_PRIVATE);
+                SharedPreferences.Editor appInstallInfoEditor = appInstallInfoSharedPref.edit();
+                appInstallInfoEditor.putString(key, value);
+                appInstallInfoEditor.apply();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * GET SHARED PREFERENCES STRING DATA
+     */
+    public static String getSharedPrefStringData(Context context, String key) {
+
+        try {
+            SharedPreferences userAcountPreference = context
+                    .getSharedPreferences(Constants.APP_PREF,
+                            Context.MODE_PRIVATE);
+            return userAcountPreference.getString(key, "");
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+}
