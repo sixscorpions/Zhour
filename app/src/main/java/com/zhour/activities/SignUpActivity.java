c3e011f65b83bb91333c98b7dd66ef1421fe042d
madhu <madhu.tota@sparity.com>
2017-07-09 09:10:38 +0530

    Ui Changes  and Update Icons

--- app/src/main/java/com/zhour/activities/SignUpActivity.java
+++ app/src/main/java/com/zhour/activities/SignUpActivity.java
@@ -11,7 +11,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.include_drawer_center);
+        setContentView(R.layout.activity_sign_up);
 
 
     }
