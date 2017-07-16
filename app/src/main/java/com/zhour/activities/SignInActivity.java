39e938151bea29e351e823ca5a0704084b12bf25
shankar <shankar.pilli@sparity.com>
2017-07-16 19:37:59 +0530

    Fixed login and signup issues

--- app/src/main/java/com/zhour/activities/SignInActivity.java
+++ app/src/main/java/com/zhour/activities/SignInActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.zhour.R;
+import com.zhour.utils.Utility;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,15 +18,21 @@ public class SignInActivity extends BaseActivity {
 
     public static final String TAG = SignInActivity.class.getSimpleName();
 
+    @BindView(R.id.tv_user_login)
+    TextView tv_user_login;
 
     @BindView(R.id.btn_login)
     Button btn_login;
-    @BindView(R.id.et_usernme)
-    EditText et_usernme;
 
+    @BindView(R.id.et_username)
+    EditText et_username;
     @BindView(R.id.et_password)
     EditText et_password;
 
+    @BindView(R.id.tv_forgot_user_id)
+    TextView tv_forgot_user_id;
+    @BindView(R.id.tv_forgot_password)
+    TextView tv_forgot_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +40,20 @@ public class SignInActivity extends BaseActivity {
         setTheme(R.style.AppTheme_NoActionBar);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-
+        setTypeFace();
     }
+
+    private void setTypeFace() {
+        tv_user_login.setTypeface(Utility.setRobotoRegular(this));
+        btn_login.setTypeface(Utility.setRobotoRegular(this));
+        et_username.setTypeface(Utility.setLucidaSansItalic(this));
+        et_password.setTypeface(Utility.setLucidaSansItalic(this));
+
+        tv_forgot_user_id.setTypeface(Utility.setLucidaSansItalic(this));
+        tv_forgot_password.setTypeface(Utility.setLucidaSansItalic(this));
+    }
+
+
     @OnClick(R.id.btn_login)
     public void loginButtonFunction() {
         Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
