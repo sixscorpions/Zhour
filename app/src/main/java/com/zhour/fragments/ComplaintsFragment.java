3b0c63b1499a3b13b026e170243cb23d2d76a9c4
Shankar Pilli <shankar.pilli@sparity.com>
2017-08-08 14:09:19 +0530

    Added complaints

--- app/src/main/java/com/zhour/fragments/ComplaintsFragment.java
+++ app/src/main/java/com/zhour/fragments/ComplaintsFragment.java
@@ -1,36 +1,57 @@
 package com.zhour.fragments;
 
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.zhour.R;
 import com.zhour.activities.DashboardActivity;
+import com.zhour.adapters.SpinnerAdapter;
 import com.zhour.aynctask.IAsyncCaller;
+import com.zhour.aynctask.ServerJSONAsyncTask;
 import com.zhour.aynctaskold.ServerIntractorAsync;
 import com.zhour.models.ComplaintModel;
+import com.zhour.models.LookUpEventsTypeModel;
 import com.zhour.models.Model;
+import com.zhour.models.SpinnerModel;
 import com.zhour.parser.ComplaintParser;
+import com.zhour.parser.LookUpEventTypeParser;
 import com.zhour.utils.APIConstants;
 import com.zhour.utils.Constants;
 import com.zhour.utils.Utility;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
-public class ComplaintsFragment extends Fragment implements IAsyncCaller{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class ComplaintsFragment extends Fragment implements IAsyncCaller {
 
     public static final String TAG = ComplaintsFragment.class.getSimpleName();
-    private DashboardActivity parent;
+    private DashboardActivity mParent;
     private View view;
+
+    @BindView(R.id.et_complaints_types)
+    EditText et_complaints_types;
+
+    private LookUpEventsTypeModel lookUpEventsTypeModel;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        parent = (DashboardActivity) getActivity();
+        mParent = (DashboardActivity) getActivity();
     }
 
     @Override
@@ -39,40 +60,80 @@ public class ComplaintsFragment extends Fragment implements IAsyncCaller{
         if (view != null)
             return view;
         view = inflater.inflate(R.layout.fragment_complaints, container, false);
-        inITUI();
+        ButterKnife.bind(this, view);
+        initUi();
         return view;
     }
 
-    private void inITUI() {
-
-
-        getComplints();
+    private void initUi() {
+        getInviteTypes();
     }
 
-    private void getComplints() {
+    private void getInviteTypes() {
+        try {
+            LinkedHashMap linkedHashMap = new LinkedHashMap();
+            linkedHashMap.put("entityname", "Complaint%20Types");
+            LookUpEventTypeParser lookUpEventTypeParser = new LookUpEventTypeParser();
+            ServerJSONAsyncTask serverJSONAsyncTask = new ServerJSONAsyncTask(
+                    mParent, Utility.getResourcesString(mParent, R.string.please_wait), true,
+                    APIConstants.GET_LOOKUP_DATA_BY_ENTITY_NAME, linkedHashMap,
+                    APIConstants.REQUEST_TYPE.POST, this, lookUpEventTypeParser);
+            Utility.execute(serverJSONAsyncTask);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        String residentID = Utility.getSharedPrefStringData(parent, Constants.RESIDENT_ID);
-        String communityID = Utility.getSharedPrefStringData(parent, Constants.COMMUNITY_ID);
+    /*This method is used to set data to spinner*/
+    @OnClick({R.id.et_complaints_types})
+    void setInviteTypeData() {
+        if (lookUpEventsTypeModel != null &&
+                lookUpEventsTypeModel.getLookUpModels() != null &&
+                lookUpEventsTypeModel.getLookUpModels().size() > 0)
+            showSpinnerDialog(getActivity(), Utility.getResourcesString(mParent, R.string.invite_type),
+                    lookUpEventsTypeModel.getLookupNames(), 1);
+    }
 
-        LinkedHashMap linkedHashMap = new LinkedHashMap();
-        linkedHashMap.put("complaintid", 0);
-        linkedHashMap.put("residentid", residentID);
-        linkedHashMap.put("communityid", communityID);
+    public void showSpinnerDialog(final Context context, final String title,
+                                  ArrayList<SpinnerModel> itemsList, final int id) {
 
-        ComplaintParser complaintParser = new ComplaintParser();
-        ServerIntractorAsync serverJSONAsyncTask = new ServerIntractorAsync(
-                parent, Utility.getResourcesString(parent, R.string.please_wait), true,
-                APIConstants.GET_COMPLINTS, linkedHashMap,
-                APIConstants.REQUEST_TYPE.POST, this, complaintParser);
-        Utility.execute(serverJSONAsyncTask);
+        AlertDialog.Builder builderSingle = new AlertDialog.Builder(context);
 
+        /*CUSTOM TITLE*/
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.layout_alert_dialog_title, null);
+        TextView tv_title = (TextView) view.findViewById(R.id.tv_alert_dialog_title);
+        RelativeLayout dialog_back_ground = (RelativeLayout) view.findViewById(R.id.dialog_back_ground);
+        dialog_back_ground.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
+        tv_title.setText(title);
+        tv_title.setTextColor(context.getResources().getColor(R.color.colorWhite));
+        builderSingle.setCustomTitle(view);
+
+
+        final SpinnerAdapter adapter = new SpinnerAdapter(context, itemsList);
+        builderSingle.setAdapter(adapter,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SpinnerModel mData = (SpinnerModel) adapter.getItem(which);
+                        if (id == 1) {
+                            String text = mData.getTitle();
+                            et_complaints_types.setText(text);
+                        }
+                    }
+                });
+        builderSingle.show();
     }
 
     @Override
     public void onComplete(Model model) {
-        if (model instanceof ComplaintModel){
+        if (model != null) {
+            if (model instanceof LookUpEventsTypeModel) {
+                lookUpEventsTypeModel = (LookUpEventsTypeModel) model;
+                if (!lookUpEventsTypeModel.isError()) {
 
+                }
+            }
         }
-
     }
 }
