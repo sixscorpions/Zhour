f15ecee9dc6216b5cac7ab59058a3563c6469ae9
Shankar Pilli <shankar.pilli@sparity.com>
2017-08-28 15:58:52 +0530

    Added emergency ripple animation

--- app/src/main/java/com/zhour/fragments/EmergencyFragment.java
+++ app/src/main/java/com/zhour/fragments/EmergencyFragment.java
@@ -2,13 +2,13 @@ package com.zhour.fragments;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.zhour.R;
 import com.zhour.activities.DashboardActivity;
@@ -22,9 +22,13 @@ public class EmergencyFragment extends Fragment {
     @BindView(R.id.img_emergency)
     ImageView img_emergency;
 
+    @BindView(R.id.content)
+    com.skyfishjy.library.RippleBackground rippleBackground;
+
     public static final String TAG = EmergencyFragment.class.getSimpleName();
     private DashboardActivity mParent;
     private View view;
+    private boolean isEmergency = false;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,6 +48,34 @@ public class EmergencyFragment extends Fragment {
     }
 
     private void initUi() {
+        isEmergency = true;
+        final Handler handler = new Handler();
+        img_emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (isEmergency) {
+                    rippleBackground.startRippleAnimation();
+                    isEmergency = false;
+                } else {
+                    rippleBackground.stopRippleAnimation();
+                    isEmergency = true;
+                }
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        callEmergency();
+                    }
+                }, 3000);
+            }
+        });
+    }
+
+    /**
+     * This method is used to call emergency
+     */
+    private void callEmergency() {
 
     }
 
